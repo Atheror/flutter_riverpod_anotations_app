4219f2088278a3b88819cc7a2a3151115fb0ac2f
Juan S. López <dir.desarrollo@whitecloud.com.co>
2023-11-21 09:43:04 -0500

    indentation

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -22,49 +22,26 @@ class _HomeScreenView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: const [
-
+        _CustomListTile(title: 'Provider Simple', subTitle: 'Un provider de sólo lectura', location: '/provider'),
+        _CustomListTile(title: 'Provider + Go_Router', subTitle: 'Un uso aplicado', location: '/provider-router'),
         _CustomListTile(
-            title: 'Provider Simple',
-            subTitle: 'Un provider de sólo lectura',
-            location: '/provider'),
-
+            title: 'State Provider', subTitle: 'Un estado para almacenar un objeto', location: '/state-provider'),
         _CustomListTile(
-            title: 'Provider + Go_Router',
-            subTitle: 'Un uso aplicado',
-            location: '/provider-router'),
-
-        _CustomListTile(
-            title: 'State Provider',
-            subTitle: 'Un estado para almacenar un objeto',
-            location: '/state-provider'),
-
-        _CustomListTile(
-            title: 'State Provider + Provider',
-            subTitle: 'TODO - Una mezcla entre providers',
-            location: '/todo'),
-
+            title: 'State Provider + Provider', subTitle: 'TODO - Una mezcla entre providers', location: '/todo'),
         _CustomListTile(
             title: 'Future Provider',
             subTitle: 'Mantener el estado de un future y su retorno',
             location: '/future-provider'),
-
         _CustomListTile(
-            title: 'Family Future Provider',
-            subTitle: 'Providers con argumentos',
-            location: '/future-family-provider'),
-
-
+            title: 'Family Future Provider', subTitle: 'Providers con argumentos', location: '/future-family-provider'),
         _CustomListTile(
             title: 'Stream Provider',
             subTitle: 'Mantener el estado de un Stream y sus emosiones',
             location: '/stream-provider'),
-
-
         _CustomListTile(
             title: 'State Notifier Provider',
             subTitle: 'Provider con notificación automática',
             location: '/state-notifier-provider'),
-
         _CustomListTile(
             title: 'Change Notifier Provider',
             subTitle: 'Provider con control de notificación a listeners',
