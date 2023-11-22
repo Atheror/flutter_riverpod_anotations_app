ee56f16e55193224d30db7a16820031bd56278f3
Juan S. López <dir.desarrollo@whitecloud.com.co>
2023-11-22 18:21:19 -0500

    future provider

---
+++ lib/presentation/providers/future_providers.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/widgets.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_anotations_app/config/config.dart';
+
+part 'future_providers.g.dart';
+
+@Riverpod(keepAlive: true)
+Future<String> pokemonName(PokemonNameRef ref) async {
+  final pokemonId = ref.watch(pokemonIdProvider);
+  final pokemonName = await PokemonInformation.getPokemonName(pokemonId);
+
+  ref.onDispose(() {
+    debugPrint('Dispose pokemonName');
+  });
+
+  return pokemonName;
+}
+
+@Riverpod(keepAlive: true)
+class PokemonId extends _$PokemonId {
+  @override
+  int build() => 1;
+
+  void nextPokemon() {
+    state++;
+  }
+
+  void prevPokemon() {
+    if (state > 1) {
+      state--;
+    }
+  }
+}
--- lib/presentation/screens/05_future_provider/future_provider_screen.dart
+++ lib/presentation/screens/05_future_provider/future_provider_screen.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_anotations_app/presentation/providers/providers.dart';
 
-class FutureProviderScreen extends StatelessWidget {
+class FutureProviderScreen extends ConsumerWidget {
   const FutureProviderScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pokemonName = ref.watch(pokemonNameProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Future Provider'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+        child: pokemonName.when(
+          data: (data) => Text(data),
+          error: (error, stackTrace) => Text('$error'),
+          loading: () => const CircularProgressIndicator.adaptive(),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.refresh),
-        onPressed: () {},
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(
+              heroTag: 'prev',
+              child: const Icon(Icons.arrow_back_outlined),
+              onPressed: () {
+                ref.read(pokemonIdProvider.notifier).prevPokemon();
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(
+              heroTag: 'next',
+              child: const Icon(Icons.arrow_forward_outlined),
+              onPressed: () {
+                ref.read(pokemonIdProvider.notifier).nextPokemon();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
